b334e5f53a7576724ac3368a4e7aed5d5a1be5f0
iqmal-1916447 <iqmal.jamaludin@gmail.com>
2022-10-26 13:48:54 +0800

    adding custom components

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+// importing custom components
+import Greet from './components/Greet';
+import ShowImage from './components/ShowImage';
+
 export default function App() {
+  const UserName = "Red Velvet Cake (Android 11)";
+  const imgSrc = require('./assets/react-vector.png');
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+        <ShowImage theSource={imgSrc} />
+        <Greet username={UserName}/>
       <Text>Copyright of RVC Group</Text>
       <StatusBar style="auto" />
     </View>
